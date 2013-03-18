1215f62efa0574d6326259bb64a883396e426353
Damijan Cavard <cavard@delo.si>
2013-03-18 11:43:33 +0100

    Added couple of bitwise operators

---
+++ bitwise/modulo/standard/modulo.java
@@ -0,0 +1,9 @@
+class modulo
+{
+   public static void main(String args[]) {
+		for(int i=0; i<10000000; i++){
+			int a= 131%4;
+		}
+   }
+}
+
---
+++ bitwise/rshift/standard/rshift.java
@@ -0,0 +1,14 @@
+public class rshift
+{
+	
+   public static void main(String args[]) {
+		int i;
+		int a=1;
+		for(i=0; i<10000000; i++){
+                       a=1;
+			a = a / 2;
+			a = a / 64;
+		}
+   }
+}
+
